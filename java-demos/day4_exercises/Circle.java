4aeeed8d491735b20da2b95fa2e1d9c31bb6fc13
Paul Enebeli <zidane138@live.fr>
2022-04-23 10:48:18 +0100

    completed the circle class

--- java-demos/day4_exercises/Circle.java
+++ java-demos/day4_exercises/Circle.java
@@ -1,4 +1,22 @@
 class Circle extends Shape{
 	
 	private double radius;
+	
+	public Circle(String name, String colour, double x, double y) {
+
+		super(name, colour, x, y);
+	}
+
+	public double getArea() {
+		return Math.PI * Math.pow(this.radius, 2);
+
+	}
+
+	public Point getCenterPoint(} {
+		Point centerPoint = new Point(super.getX(), super.getY());
+		//I assumed the origin of our circle was our center of circle
+		//need to ask Kishore what does th epoint coords in shape class model/represent
+	}
 }
+
+
